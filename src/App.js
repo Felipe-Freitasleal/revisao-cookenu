da591e0667f8354199555d595140a934d13ea763
yuzo <yuzokamoto@gmail.com>
2022-12-07 16:10:36 -0300

    header finalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import Router from './routes/Router'
+import { GlobalStyle } from './GlobalStyle.styled'
 
 const App = () => {
   return (
     <>
         <ChakraProvider>
+            <GlobalStyle />
             <Router />
         </ChakraProvider>
     </>
